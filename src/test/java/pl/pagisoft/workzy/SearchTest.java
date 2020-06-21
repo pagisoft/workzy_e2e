231a1c07d0aea14158c37cb48070cece0bceda8b
Grzegorz Macialek <grzegorzmacialek@gmail.com>
2020-06-21 10:02:34 +0200

    BasicSearchWithValidInputs added

--- src/test/java/pl/pagisoft/workzy/SearchTest.java
+++ src/test/java/pl/pagisoft/workzy/SearchTest.java
@@ -12,8 +12,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class BasicSearchTest {
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
+public class SearchTest {
     private WebDriver driver;
     private Map<String, Object> vars;
     JavascriptExecutor js;
@@ -22,10 +24,6 @@ public class BasicSearchTest {
     public void setUp() {
 
         System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
-
-        System.out.print(
-                "\n _____         _         _____ _             _           _ \n|_   _|       | |       /  ___| |           | |         | |\n  | | ___  ___| |_ ___  \\ `--.| |_ __ _ _ __| |_ ___  __| |\n  | |/ _ \\/ __| __/ __|  `--. \\ __/ _` | '__| __/ _ \\/ _` |\n  | |  __/\\__ \\ |_\\__ \\ /\\__/ / || (_| | |  | ||  __/ (_| |\n  \\_/\\___||___/\\__|___/ \\____/ \\__\\__,_|_|   \\__\\___|\\__,_|\n                                                           \n                                                           \n"
-        );
 
         driver = new ChromeDriver();
         js = (JavascriptExecutor) driver;
@@ -38,7 +36,7 @@ public class BasicSearchTest {
     }
 
     @Test
-    public void Search_With_Empty_Inputs_Test() {
+    public void basicSearchWithEmptyInputs() {
         driver.get("https://workdemo.me/workzy/");
         driver.manage().window().setSize(new Dimension(1079, 964));
         {
@@ -57,10 +55,34 @@ public class BasicSearchTest {
         {
             List<WebElement> elements = driver.findElements(By.cssSelector(".search_keywords > #search_keywords"));
             assert (elements.size() > 0);
+        }
+    }
 
-            System.out.print(
-                    " _______        _         _____                       _ \n |__   __|      | |       |  __ \\                     | |\n    | | ___  ___| |_ ___  | |__) |_ _ ___ ___  ___  __| |\n    | |/ _ \\/ __| __/ __| |  ___/ _` / __/ __|/ _ \\/ _` |\n    | |  __/\\__ \\ |_\\__ \\ | |  | (_| \\__ \\__ \\  __/ (_| |\n    |_|\\___||___/\\__|___/ |_|   \\__,_|___/___/\\___|\\__,_|\n                                                         \n                                                         \n"
-            );
+    @Test
+    public void basicSearchWithValidInputs() {
+        driver.get("https://workdemo.me/workzy/");
+        driver.manage().window().setSize(new Dimension(1079, 964));
+        {
+            WebDriverWait wait = new WebDriverWait(driver, 30);
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.id("intro-keywords")));
+        }
+        driver.findElement(By.id("intro-keywords")).click();
+        driver.findElement(By.id("intro-keywords")).sendKeys("test");
+        driver.findElement(By.id("search_location")).click();
+        driver.findElement(By.id("search_location")).sendKeys("test");
+        driver.findElement(By.id("select2-search_category-container")).click();
+        driver.findElement(By.cssSelector("i:nth-child(2)")).click();
+        {
+            WebDriverWait wait = new WebDriverWait(driver, 30);
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".search_keywords > #search_keywords")));
+        }
+        {
+            String value = driver.findElement(By.cssSelector(".search_keywords > #search_keywords")).getAttribute("value");
+            assertThat(value, is("test"));
+        }
+        {
+            String value = driver.findElement(By.id("search_location")).getAttribute("value");
+            assertThat(value, is("test"));
         }
     }
 }
